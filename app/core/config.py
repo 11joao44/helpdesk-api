e318d531d426d785227c2f63d8099ec3c4307e75
João Pedro (Dev) <qualidade.09@carvalima.com.br>
2025-11-26 11:55:26 -0400

    Feauture: corrgindo variaveis de sistema

--- app/core/config.py
+++ app/core/config.py
@@ -6,21 +6,34 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 logger.remove()
 logger.add(stderr, level="DEBUG", colorize=True, format="<green>{time:HH:mm:ss}</green> [<level>{level}</level>][<blue>{name}</blue>]<green>{function}</green>><green>{line}</green>: <level>{message}</level>")
 
-class Settings(BaseSettings):
-    BITRIX_WEBHOOK_URL: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    TIMEOUT_REQUEST: int = 15.0
-    PG_CARVALIMA_HELPDESK_DBNAME: str
-    PG_BOTAPP_PASSWORD: str
-    PG_BOTAPP_HOST: str
-    PG_BOTAPP_USER: str
-    PG_BOTAPP_PORT: str
 
-    model_config = SettingsConfigDict(env_file=None, env_file_encoding="utf-8")
+
+# class Settings(BaseSettings):
+#     BITRIX_WEBHOOK_URL: str
+#     ALGORITHM: str = "HS256"
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+#     TIMEOUT_REQUEST: int = 15.0
+#     PG_CARVALIMA_HELPDESK_DBNAME: str
+#     PG_BOTAPP_PASSWORD: str
+#     PG_BOTAPP_HOST: str
+#     PG_BOTAPP_USER: str
+#     PG_BOTAPP_PORT: str
+
+#     model_config = SettingsConfigDict(env_file=None, env_file_encoding="utf-8")
 
 try:
-    settings = Settings()
+    settings =  {
+        "BITRIX_WEBHOOK_URL": getenv("BITRIX_WEBHOOK_URL"),
+        "ALGORITHM": "HS256",
+        "TIMEOUT_REQUEST" : 15.0,
+        "ACCESS_TOKEN_EXPIRE_MINUTES": 30,
+        "PG_CARVALIMA_HELPDESK_DBNAME": getenv("PG_CARVALIMA_HELPDESK_DBNAME"),
+        "PG_BOTAPP_HOST": getenv("PG_BOTAPP_HOST"),
+        "PG_BOTAPP_PASSWORD": getenv("PG_BOTAPP_PASSWORD"),
+        "PG_BOTAPP_PORT": getenv("PG_BOTAPP_PORT"),
+        "PG_BOTAPP_USER": getenv("PG_BOTAPP_USER"),
+    }
+
     DATABASE_URL = f"postgresql+asyncpg://{settings.PG_BOTAPP_USER}:{quote_plus(settings.PG_BOTAPP_PASSWORD)}@{settings.PG_BOTAPP_HOST}:{settings.PG_BOTAPP_PORT}/{settings.PG_CARVALIMA_HELPDESK_DBNAME}"
 except Exception as e:
     logger.error(f"Erro carregando Settings (Variável de ambiente faltando?): {e}")
