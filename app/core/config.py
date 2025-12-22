3ce2ae0066790c40220cfe63b7b025b5efcb7c7e
João Pedro (Dev) <qualidade.09@carvalima.com.br>
2025-12-22 11:36:38 -0400

    feat: Add a core configuration module for managing application settings and update the Bitrix inbound URL environment variable in docker-compose.

--- app/core/config.py
+++ app/core/config.py
@@ -7,14 +7,18 @@ from os import getenv
 load_dotenv()
 
 logger.remove()
-logger.add(stderr, level="DEBUG", colorize=True, format="<green>{time:HH:mm:ss}</green> [<level>{level}</level>][<blue>{name}</blue>]<green>{function}</green>><green>{line}</green>: <level>{message}</level>")
+logger.add(
+    stderr,
+    level="DEBUG",
+    colorize=True,
+    format="<green>{time:HH:mm:ss}</green> [<level>{level}</level>][<blue>{name}</blue>]<green>{function}</green>><green>{line}</green>: <level>{message}</level>",
+)
 
 try:
-    settings =  {
-        "BITRIX_WEBHOOK_URL": getenv("BITRIX_WEBHOOK_URL"),
+    settings = {
         "ALGORITHM": "HS256",
         "SECRET_KEY": "564546897789879asd",
-        "TIMEOUT_REQUEST" : 15.0,
+        "TIMEOUT_REQUEST": 15.0,
         "ACCESS_TOKEN_EXPIRE_MINUTES": 30,
         "PG_CARVALIMA_HELPDESK_DBNAME": getenv("PG_CARVALIMA_HELPDESK_DBNAME"),
         "PG_BOTAPP_HOST": getenv("PG_BOTAPP_HOST"),
@@ -23,7 +27,6 @@ try:
         "PG_BOTAPP_USER": getenv("PG_BOTAPP_USER"),
         "WEBMAIL_USUARIO": getenv("WEBMAIL_USUARIO"),
         "WEBMAIL_SENHA": getenv("WEBMAIL_SENHA"),
-        "BITRIX_WEBHOOK_TOKEN": getenv("BITRIX_WEBHOOK_TOKEN"),
         "BITRIX_INBOUND_URL": getenv("BITRIX_INBOUND_URL"),
     }
     DATABASE_URL = f"postgresql+asyncpg://{settings["PG_BOTAPP_USER"]}:{quote_plus(settings["PG_BOTAPP_PASSWORD"])}@{settings["PG_BOTAPP_HOST"]}:{settings["PG_BOTAPP_PORT"]}/{settings["PG_CARVALIMA_HELPDESK_DBNAME"]}"
