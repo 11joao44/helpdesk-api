d0504bf31c832c443b8807f7abc248743225950d
João Pedro (Dev) <qualidade.09@carvalima.com.br>
2026-01-15 12:59:00 -0400

    feat: Implement multi-database connection with failover logic and dynamic configuration loading.

--- app/core/config.py
+++ app/core/config.py
@@ -17,14 +17,27 @@ logger.add(
 try:
     settings = {
         "ALGORITHM": "HS256",
-        "SECRET_KEY": "564546897789879asd",
+        "SECRET_KEY": getenv("HELPDESK_SECRET_KEY"),
         "TIMEOUT_REQUEST": 15.0,
         "ACCESS_TOKEN_EXPIRE_MINUTES": 30,
-        "PG_CARVALIMA_HELPDESK_DBNAME": getenv("PG_CARVALIMA_HELPDESK_DBNAME"),
-        "PG_BOTAPP_HOST": getenv("PG_BOTAPP_HOST"),
-        "PG_BOTAPP_PASSWORD": getenv("PG_BOTAPP_PASSWORD"),
-        "PG_BOTAPP_PORT": getenv("PG_BOTAPP_PORT"),
-        "PG_BOTAPP_USER": getenv("PG_BOTAPP_USER"),
+        "HELPDESK_DB_DBNAME_1": getenv("HELPDESK_DB_DBNAME_1"),
+        "HELPDESK_DB_HOST_1": getenv("HELPDESK_DB_HOST_1"),
+        "HELPDESK_DB_USER_1": getenv("HELPDESK_DB_USER_1"),
+        "HELPDESK_DB_PASSWORD_1": getenv("HELPDESK_DB_PASS_1"),
+        "HELPDESK_DB_PORT_1": getenv("HELPDESK_DB_PORT_1"),
+
+        "HELPDESK_DB_DBNAME_2": getenv("HELPDESK_DB_DBNAME_2"),
+        "HELPDESK_DB_HOST_2": getenv("HELPDESK_DB_HOST_2"),
+        "HELPDESK_DB_USER_2": getenv("HELPDESK_DB_USER_2"),
+        "HELPDESK_DB_PASSWORD_2": getenv("HELPDESK_DB_PASS_2"),
+        "HELPDESK_DB_PORT_2": getenv("HELPDESK_DB_PORT_2"),
+
+        "HELPDESK_DB_DBNAME_3": getenv("HELPDESK_DB_DBNAME_3"),
+        "HELPDESK_DB_HOST_3": getenv("HELPDESK_DB_HOST_3"),
+        "HELPDESK_DB_USER_3": getenv("HELPDESK_DB_USER_3"),
+        "HELPDESK_DB_PASSWORD_3": getenv("HELPDESK_DB_PASS_3"),
+        "HELPDESK_DB_PORT_3": getenv("HELPDESK_DB_PORT_3"),
+
         "WEBMAIL_USUARIO": getenv("WEBMAIL_USUARIO"),
         "WEBMAIL_SENHA": getenv("WEBMAIL_SENHA"),
         "BITRIX_INBOUND_URL": getenv("BITRIX_INBOUND_URL"),
@@ -32,7 +45,17 @@ try:
         "MINIO_ACCESS_KEY": getenv("MINIO_USER"),
         "MINIO_SECRET_KEY": getenv("MINIO_SENHA"),
     }
-    DATABASE_URL = f"postgresql+asyncpg://{settings["PG_BOTAPP_USER"]}:{quote_plus(settings["PG_BOTAPP_PASSWORD"])}@{settings["PG_BOTAPP_HOST"]}:{settings["PG_BOTAPP_PORT"]}/{settings["PG_CARVALIMA_HELPDESK_DBNAME"]}"
+    
+    DATABASE_CONFIGS = []
+    for i in range(1, 4):
+        if settings.get(f"HELPDESK_DB_HOST_{i}"):
+            DATABASE_CONFIGS.append({
+                "user": settings[f"HELPDESK_DB_USER_{i}"],
+                "password": settings[f"HELPDESK_DB_PASSWORD_{i}"],
+                "host": settings[f"HELPDESK_DB_HOST_{i}"],
+                "port": settings[f"HELPDESK_DB_PORT_{i}"],
+                "dbname": settings[f"HELPDESK_DB_DBNAME_{i}"],
+            })
 except Exception as e:
     logger.error(f"Erro carregando Settings (Variável de ambiente faltando?): {e}")
     raise
